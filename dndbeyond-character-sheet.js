21d0d9e05be88b18a78bc263ce093d9ceef2c2fe
jopeek <jopeek@gmail.com>
2021-12-20 09:53:44 -0800

    1.8.1
    - fix attuned indicator
    - show only equipped and attuned (if required) items in Actions tab
    - code formatting cleanup

--- dndbeyond-character-sheet.js
+++ dndbeyond-character-sheet.js
@@ -2,35 +2,35 @@ import ActorSheet5eCharacter from "../../systems/dnd5e/module/actor/sheets/chara
 
 class DNDBeyondCharacterSheet5e extends ActorSheet5eCharacter {
   constructor(...args) {
-      super(...args);
-  
-      /**
-       * Track the set of item filters which are applied
-       * @type {Set}
-       */
-      this._filters = {
-        inventory: new Set(),
-        spellbook: new Set(),
-        features: new Set(),
-        actions: new Set()
-      };
+    super(...args);
+
+    /**
+     * Track the set of item filters which are applied
+     * @type {Set}
+     */
+    this._filters = {
+      inventory: new Set(),
+      spellbook: new Set(),
+      features: new Set(),
+      actions: new Set()
+    };
   }
-  
+
 
   get template() {
-      if ( !game.user.isGM && this.actor.limited ) return "systems/dnd5e/templates/actors/limited-sheet.html";
-      return "modules/dndbeyond-character-sheet/template/dndbeyond-character-sheet.html";
+    if (!game.user.isGM && this.actor.limited) return "systems/dnd5e/templates/actors/limited-sheet.html";
+    return "modules/dndbeyond-character-sheet/template/dndbeyond-character-sheet.html";
   }
 
   static get defaultOptions() {
-      const options = super.defaultOptions;
+    const options = super.defaultOptions;
 
-      mergeObject(options, {
-          classes: ["dnd5e", "sheet", "actor", "character", "dndbcs"],
-          width: 1220,
-          height: 940
-      });
-      return options;
+    mergeObject(options, {
+      classes: ["dnd5e", "sheet", "actor", "character", "dndbcs"],
+      width: 1220,
+      height: 940
+    });
+    return options;
   }
 
   /**
@@ -40,12 +40,12 @@ class DNDBeyondCharacterSheet5e extends ActorSheet5eCharacter {
   _initializeFilterItemList(i, ul) {
     const set = this._filters[ul.dataset.filter];
     const filters = ul.querySelectorAll(".filter-item");
-    for ( let li of filters ) {
-      if ( set.has(li.dataset.filter) ) li.classList.add("active");
+    for (let li of filters) {
+      if (set.has(li.dataset.filter)) li.classList.add("active");
     }
   }
 
-  
+
 
   /**
    * Organize and classify Owned Items for Character sheets
@@ -55,12 +55,48 @@ class DNDBeyondCharacterSheet5e extends ActorSheet5eCharacter {
 
     // Categorize items as inventory, spellbook, features, and classes
     const inventory = {
-      weapon: { label: "DND5E.ItemTypeWeaponPl", items: [], dataset: {type: "weapon"} },
-      equipment: { label: "DND5E.ItemTypeEquipmentPl", items: [], dataset: {type: "equipment"} },
-      consumable: { label: "DND5E.ItemTypeConsumablePl", items: [], dataset: {type: "consumable"} },
-      tool: { label: "DND5E.ItemTypeToolPl", items: [], dataset: {type: "tool"} },
-      backpack: { label: "DND5E.ItemTypeContainerPl", items: [], dataset: {type: "backpack"} },
-      loot: { label: "DND5E.ItemTypeLootPl", items: [], dataset: {type: "loot"} }
+      weapon: {
+        label: "DND5E.ItemTypeWeaponPl",
+        items: [],
+        dataset: {
+          type: "weapon"
+        }
+      },
+      equipment: {
+        label: "DND5E.ItemTypeEquipmentPl",
+        items: [],
+        dataset: {
+          type: "equipment"
+        }
+      },
+      consumable: {
+        label: "DND5E.ItemTypeConsumablePl",
+        items: [],
+        dataset: {
+          type: "consumable"
+        }
+      },
+      tool: {
+        label: "DND5E.ItemTypeToolPl",
+        items: [],
+        dataset: {
+          type: "tool"
+        }
+      },
+      backpack: {
+        label: "DND5E.ItemTypeContainerPl",
+        items: [],
+        dataset: {
+          type: "backpack"
+        }
+      },
+      loot: {
+        label: "DND5E.ItemTypeLootPl",
+        items: [],
+        dataset: {
+          type: "loot"
+        }
+      }
     };
 
     // Partition items by category
@@ -74,18 +110,23 @@ class DNDBeyondCharacterSheet5e extends ActorSheet5eCharacter {
       item.hasUses = item.data.uses && (item.data.uses.max > 0);
       item.isOnCooldown = item.data.recharge && !!item.data.recharge.value && (item.data.recharge.charged === false);
       item.isDepleted = item.isOnCooldown && (item.data.uses.per && (item.data.uses.value > 0));
-      item.hasTarget = !!item.data.target && !(["none",""].includes(item.data.target.type));
+      item.hasTarget = !!item.data.target && !(["none", ""].includes(item.data.target.type));
 
       // Item toggle state
       this._prepareItemToggleState(item);
 
       // Classify items into types
-      if ( item.type === "spell" ) arr[1].push(item);
-      else if ( item.type === "feat" ) arr[2].push(item);
-      else if ( item.type === "class" ) arr[3].push(item);
-      else if ( Object.keys(inventory).includes(item.type ) ) arr[0].push(item);
+      if (item.type === "spell") arr[1].push(item);
+      else if (item.type === "feat") arr[2].push(item);
+      else if (item.type === "class") arr[3].push(item);
+      else if (Object.keys(inventory).includes(item.type)) arr[0].push(item);
       return arr;
-    }, [[], [], [], []]);
+    }, [
+      [],
+      [],
+      [],
+      []
+    ]);
 
     // Apply active item filters
     items = this._filterItems(items, this._filters.inventory);
@@ -95,19 +136,19 @@ class DNDBeyondCharacterSheet5e extends ActorSheet5eCharacter {
 
     //********************************************************************************** THIS IS THE SORTING CODE *****************************************************/*
     items = items.sort(function (a, b) {
-        return a.name.localeCompare( b.name );
-      });
-      spells = spells.sort(function (a, b) {
-        return a.name.localeCompare( b.name );
-      });
-      feats = feats.sort(function (a, b) {
-        return a.name.localeCompare( b.name );
-      });
-      //********************************************************************************** END SORTING CODE **************************************************************/
-  
+      return a.name.localeCompare(b.name);
+    });
+    spells = spells.sort(function (a, b) {
+      return a.name.localeCompare(b.name);
+    });
+    feats = feats.sort(function (a, b) {
+      return a.name.localeCompare(b.name);
+    });
+    //********************************************************************************** END SORTING CODE **************************************************************/
+
 
     // Organize items
-    for ( let i of items ) {
+    for (let i of items) {
       i.data.quantity = i.data.quantity || 0;
       i.data.weight = i.data.weight || 0;
       i.totalWeight = Math.round(i.data.quantity * i.data.weight * 10) / 10;
@@ -122,12 +163,35 @@ class DNDBeyondCharacterSheet5e extends ActorSheet5eCharacter {
 
     // Organize Features
     const features = {
-      classes: { label: "DND5E.ItemTypeClassPl", items: [], hasActions: false, dataset: {type: "class"}, isClass: true },
-      active: { label: "DND5E.FeatureActive", items: [], hasActions: true, dataset: {type: "feat", "activation.type": "action"} },
-      passive: { label: "DND5E.FeaturePassive", items: [], hasActions: false, dataset: {type: "feat"} }
+      classes: {
+        label: "DND5E.ItemTypeClassPl",
+        items: [],
+        hasActions: false,
+        dataset: {
+          type: "class"
+        },
+        isClass: true
+      },
+      active: {
+        label: "DND5E.FeatureActive",
+        items: [],
+        hasActions: true,
+        dataset: {
+          type: "feat",
+          "activation.type": "action"
+        }
+      },
+      passive: {
+        label: "DND5E.FeaturePassive",
+        items: [],
+        hasActions: false,
+        dataset: {
+          type: "feat"
+        }
+      }
     };
-    for ( let f of feats ) {
-      if ( f.data.activation.type ) features.active.items.push(f);
+    for (let f of feats) {
+      if (f.data.activation.type) features.active.items.push(f);
       else features.passive.items.push(f);
     }
     classes.sort((a, b) => b.levels - a.levels);
@@ -143,58 +207,65 @@ class DNDBeyondCharacterSheet5e extends ActorSheet5eCharacter {
   /* -------------------------------------------- */
 
   getData() {
-      const sheetData = super.getData();
+    const sheetData = super.getData();
 
-      // Temporary HP
-      let hp = sheetData.data.attributes.hp;
-      if (hp.temp === 0) delete hp.temp;
-      if (hp.tempmax === 0) delete hp.tempmax;
-  
-      // Resources
-      sheetData["resources"] = ["primary", "secondary", "tertiary"].reduce((arr, r) => {
-        const res = sheetData.data.resources[r] || {};
-        res.name = r;
-        res.placeholder = game.i18n.localize("DND5E.Resource"+r.titleCase());
-        if (res && res.value === 0) delete res.value;
-        if (res && res.max === 0) delete res.max;
-        return arr.concat([res]);
-      }, []);
-  
-      // Experience Tracking
-      sheetData["disableExperience"] = game.settings.get("dnd5e", "disableExperienceTracking");
-  
-      //console.log("DNDBeyond-Character-Sheet | sheetData", sheetData);
+    // Temporary HP
+    let hp = sheetData.data.attributes.hp;
+    if (hp.temp === 0) delete hp.temp;
+    if (hp.tempmax === 0) delete hp.tempmax;
 
-      // let actor = game.actors.get(sheetData.actor._id);
+    // Resources
+    sheetData["resources"] = ["primary", "secondary", "tertiary"].reduce((arr, r) => {
+      const res = sheetData.data.resources[r] || {};
+      res.name = r;
+      res.placeholder = game.i18n.localize("DND5E.Resource" + r.titleCase());
+      if (res && res.value === 0) delete res.value;
+      if (res && res.max === 0) delete res.max;
+      return arr.concat([res]);
+    }, []);
 
-      // const conditions = canvas.tokens.get(actor.getActiveTokens()[0].data._id).data.effects;
-      // let testdata = conditions;
+    // Experience Tracking
+    sheetData["disableExperience"] = game.settings.get("dnd5e", "disableExperienceTracking");
 
-      // console.log("DNDBeyond-Character-Sheet | testdata", testdata);
-      
-      // Return data for rendering
-      return sheetData;
-    }
+    console.log("DNDBeyond-Character-Sheet | sheetData", sheetData);
 
-  
+    // let actor = game.actors.get(sheetData.actor._id);
+
+    // const conditions = canvas.tokens.get(actor.getActiveTokens()[0].data._id).data.effects;
+    // let testdata = conditions;
+
+    // console.log("DNDBeyond-Character-Sheet | testdata", testdata);
+
+    // Return data for rendering
+    return sheetData;
+  }
+
+
 }
 
 Hooks.once('init', async function () {
 
   Handlebars.registerHelper('ifeq', function (a, b, options) {
-      if (a == b) { return options.fn(this); }
-      return options.inverse(this);
+    if (a == b) {
+      return options.fn(this);
+    }
+    return options.inverse(this);
   });
-  
+
   Handlebars.registerHelper('ifnoteq', function (a, b, options) {
-      if (a != b) { return options.fn(this); }
-      return options.inverse(this);
+    if (a != b) {
+      return options.fn(this);
+    }
+    return options.inverse(this);
   });
-  
+
   console.log("DNDBeyond-Character-Sheet | Loaded");
 
-  Actors.registerSheet('dnd5e', DNDBeyondCharacterSheet5e, {types: ['character'], makeDefault: false});
-  
+  Actors.registerSheet('dnd5e', DNDBeyondCharacterSheet5e, {
+    types: ['character'],
+    makeDefault: false
+  });
+
 });
 
 Hooks.on('ready', () => {
@@ -205,5 +276,5 @@ Hooks.on('ready', () => {
   } catch (error) {
     console.log("DNDBeyond-Character-Sheet | Better Rolls 5e module not installed - no big deal, carry on!");
   }
-  
+
 });
